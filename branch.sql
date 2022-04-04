e9c86ee89283c49c582a1d0bed31410c16b34ccb
Christina Song <christina.song202@gmail.com>
2022-04-03 23:03:35 -0400

    employee table, fixed branch by adding rowID

--- branch.sql
+++ branch.sql
@@ -1,26 +1,24 @@
 CREATE TABLE Branch (
+    rowID INT NOT NULL,
 	branchID INT NOT NULL,
     city VARCHAR(20),
     managerID INT,
     dentistID INT,
     hygienistID INT,
     receptionistID INT,
-    PRIMARY KEY (branchID),
+    PRIMARY KEY (rowID),
     FOREIGN KEY (managerID) REFERENCES Employee(employeeID),
     FOREIGN KEY (dentistID) REFERENCES Employee(employeeID),
     FOREIGN KEY (hygienistID) REFERENCES Employee(employeeID),
     FOREIGN KEY (receptionistID) REFERENCES Employee(employeeID)
 );
 
-INSERT INTO Branch (branchID, city, managerID, dentistID, hygienistID, receptionistID)
-VALUES ('Sandy Hill', 'Ottawa', 1, 1, 1, 1),
-('Sandy Hill', 'Ottawa', NULL, 2, 2, 2),
-('Sandy Hill', 'Ottawa', NULL, 3, 3, NULL),
-('Sandy Hill', 'Ottawa', NULL, 4, 4, NULL),
-('Sandy Hill', 'Ottawa', NULL, 5, 5, NULL),
-('Downtown', 'Toronto', 6, 6, 6, 6),
-('Downtown', 'Toronto', NULL, 7, 7, NULL),
-('Downtown', 'Toronto', NULL, 8, 8, NULL),
-('Downtown', 'Toronto', NULL, 9, 9, NULL),
-('Downtown', 'Toronto', NULL, 10, 10, NULL),
-('Downtown', 'Toronto', NULL, 11, 11, NULL);
+INSERT INTO Branch (rowID, branchID, city, managerID, dentistID, hygienistID, receptionistID)
+VALUES (1, 1, 'Ottawa', 1, 4, 7, 2),
+(2, 1, 'Ottawa', NULL, 5, 8, 3),
+(3, 1, 'Ottawa', NULL, 6, NULL, NULL),
+(4, 2, 'Toronto', 9, 11, 13, 10),
+(5, 2, 'Toronto', NULL, 12, 14, NULL),
+(6, 2, 'Toronto', NULL, NULL, 15, NULL);
+
+SELECT * FROM Branch LIMIT 1000;
