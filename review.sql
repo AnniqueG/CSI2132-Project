29d24a947623e7beafd3ef2619313eff3f722433
Christina Song <christina.song202@gmail.com>
2022-04-09 18:48:17 -0400

    updated user, employee, and patient so employee and patient reference user. not sure why review doesn't work

---
+++ review.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Review (
+    reviewID INT NOT NULL,
+    branchID INT NOT NULL,
+    professionalism VARCHAR(20),
+    communication VARCHAR(20),
+    cleanliness VARCHAR(20),
+    value VARCHAR(20),
+    PRIMARY KEY (reviewID),
+    FOREIGN KEY (branchID) REFERENCES Branch(branchID)
+);
+
+INSERT INTO Review (reviewID, branchID, professionalism, communication, cleanliness, value)
+VALUES (10, 1, "great", "good", "excellent", "good"),
+(11, 1, "ok", "good", "great", "bad"),
+(12, 1, "excellent", "good", "great", "ok"),
+(13, 2, "exceptional", "ok", "bad", "good"),
+(14, 2, "bad", "good", "ok", "ok");
--- user.sql
+++ user.sql
@@ -10,6 +10,25 @@ CREATE TABLE User (
     PRIMARY KEY (userID)
 );
 
+-- employees
+INSERT INTO User (userID, SSN, name, address, gender, email, date_of_birth, phone_number)
+VALUES (8, 999888777, 'Camille Perez', 'Ottawa', 'female', 'c.pere@clinic.com', '1990-08-09', '6138731234'),
+(9, 333222555, 'Max Verstappen', 'Ottawa', 'male', 'm.vers@clinic.com', '1991-07-12', '6139991289'),
+(10, 444123444, 'Sergio Perez', 'Ottawa', 'male', 's.pere@clinic.com', '1989-12-03', '6139990012'),
+(11, 666123555, 'Mikasa Ackerman', 'Ottawa', 'female', 'm.acke@clinic.com', '1993-00-12', '6139990809'),
+(12, 888999000, 'Levi Ackerman', 'Ottawa', 'male', 'l.acke@clinic.com', '1990-12-07', '6134445657'),
+(13, 999000111, 'Cassie Sullivan', 'Ottawa', 'female', 'c.sull@clinic.com', '1991-09-12', '6134445678'),
+(14, 888111222, 'Ben Parish', 'Ottawa', 'male', 'b.pari@clinic.com', '1992-10-13', '6134446789'),
+(15, 333666222, 'Katniss Everdeen', 'Ottawa', 'female', 'k.ever@clinic.com','1988-01-09', '6138781234'),
+(16, 777111444, 'Primrose Everdeen', 'Toronto', 'female', 'p.ever@clinic.com', '1989-02-23', '4370009192'),
+(17, 666777000, 'Peeta Melark', 'Toronto', 'male', 'p.mela@clinic.com', '1987-09-10', '4167771256'),
+(18, 999444111, 'Gale Hawthorn', 'Toronto', 'male', 'g.hawt@clinic.com', '1986-09-24', '4167871254'),
+(19, 666777222, 'Rue Bennet', 'Toronto', 'female', 'r.benn@clinic.com', '1977-12-07', '4378881236'),
+(20, 888000444, 'Nate Jacobs', 'Toronto', 'male', 'n.jaco@clinic.com', '1982-08-05', '4168889876'),
+(21, 000333777, 'Cassie Howard', 'Toronto', 'female', 'c.howa@clinic.com', '1986-09-18', '4167773344'),
+(22, 111999000, 'Maddy Perez', 'Toronto', 'female', 'm.pere@clinic.com', '1983-08-19', '4378889893');
+
+-- patients
 INSERT INTO User (userID, SSN, name, address, gender, email, date_of_birth, phone_number)
 VALUES (1, 123456789, 'John Smith', 'Ottawa', 'male', 'john.s@gmail.com', '2000-01-03', '6131231234'),
 (2, 111222333, 'Jane Brown', 'Ottawa', 'female', 'jane.b@gmail.com', '1990-02-28', '6130001234'),
