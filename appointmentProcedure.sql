6d1d1fc5581652c64e1a9bea6e8ec99a55e31821
dray226 <55163854+dray226@users.noreply.github.com>
2022-04-15 18:27:54 -0400

    single quotes

--- appointmentProcedure.sql
+++ appointmentProcedure.sql
@@ -11,8 +11,8 @@ CREATE TABLE appointmentProcedure (
 );
 INSERT INTO appointmentProcedure (apptProcID,procedure_code,procedure_type,descr,teeth_involved,flat_cost,insurance_coverage,net_charge)
 VALUES
-(0,"CLN","cleaning", "Routine Professional Cleaning", "All", 50, 0, 50),
-(2, "RTC","root canal","Infectious Tissue Removal", "Infected Tooth", 295, 195, 100),
-(5, "VNR", "veneers","Thin Covering on Teeth","Crooked or Discoloured Teeth", 350, 225, 125),
-(8, "FLL", "fillings", "Filling Procedure for Enamel Preservation", "Decayed Teeth", 680, 445, 235),
-(17, "EXT", "extractions", "Removal of Problematic Tooth", "Any", 625, 375, 250);
+(0,'CLN','cleaning', 'Routine Professional Cleaning', 'All', 50, 0, 50),
+(2, 'RTC','root canal','Infectious Tissue Removal', 'Infected Tooth', 295, 195, 100),
+(5, 'VNR', 'veneers','Thin Covering on Teeth','Crooked or Discoloured Teeth', 350, 225, 125),
+(8, 'FLL', 'fillings', 'Filling Procedure for Enamel Preservation', 'Decayed Teeth', 680, 445, 235),
+(17, 'EXT', 'extractions', 'Removal of Problematic Tooth', 'Any', 625, 375, 250);
