174d08cc4979263ae834ef0faf0d47650eb1a0f3
Christina Song <christina.song202@gmail.com>
2022-04-15 10:52:05 -0400

    fixed mistake in patient insertion

--- patient.sql
+++ patient.sql
@@ -7,6 +7,6 @@ CREATE TABLE Patient (
     FOREIGN KEY userID REFERENCES User(userID)
 );
 
-INSERT INTO Patient (patientID, insurance)
+INSERT INTO Patient (patientID, userID, insurance)
 VALUES (100, 1, 1001), (101, 2, 1002), (102, 3, 1003), (103, 4, 1004),  -- ottawa branch
 (104, 5, 1005), (105, 6, 1006), (106, 7, 1007); -- toronto branch
