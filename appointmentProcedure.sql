0e604e740241288b0bbf9e6ce566a80c226a246e
Yassine Sami <92585708+yasssami@users.noreply.github.com>
2022-04-15 19:43:11 -0400

    Swapped Misaligned Rows

--- appointmentProcedure.sql
+++ appointmentProcedure.sql
@@ -4,15 +4,15 @@ CREATE TABLE appointmentProcedure (
     procedure_type VARCHAR(20),
     descr VARCHAR(50),
     teeth_involved VARCHAR(50),
-    flat_cost INT,
-    insurance_coverage INT,
-    net_charge INT,
-    PRIMARY KEY (apptProcID)
+    flat_cost FLOAT,
+    insurance_coverage FLOAT,
+    net_charge FLOAT,
+    PRIMARY KEY (apptProcID),
 );
 INSERT INTO appointmentProcedure (apptProcID,procedure_code,procedure_type,descr,teeth_involved,flat_cost,insurance_coverage,net_charge)
 VALUES
 (0,'CLN','cleaning', 'Routine Professional Cleaning', 'All', 50, 0, 50),
-(2, 'RTC','root canal','Infectious Tissue Removal', 'Infected Tooth', 295, 195, 100),
-(5, 'VNR', 'veneers','Thin Covering on Teeth','Crooked or Discoloured Teeth', 350, 225, 125),
-(8, 'FLL', 'fillings', 'Filling Procedure for Enamel Preservation', 'Decayed Teeth', 680, 445, 235),
-(17, 'EXT', 'extractions', 'Removal of Problematic Tooth', 'Any', 625, 375, 250);
+(2, 'RTC','root canal','Infectious Tissue Removal', 'Infected Tooth', 295, 100, 195),
+(5, 'VNR', 'veneers','Thin Covering on Teeth','Crooked or Discoloured Teeth', 350, 125, 225),
+(8, 'FLL', 'fillings', 'Filling Procedure for Enamel Preservation', 'Decayed Teeth', 680, 235, 445),
+(17, 'EXT', 'extractions', 'Removal of Problematic Tooth', 'Any', 625, 250, 375);
